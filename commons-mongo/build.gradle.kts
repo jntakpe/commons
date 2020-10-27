f9b67d67d699ef768354bffb47b70d2b64b28284
jntakpe <jntakpe@gmail.com>
2020-10-27 15:19:09 +0100

    build: use released version of KMongo async instead of KMongo reactor snapshot

--- commons-mongo/build.gradle.kts
+++ commons-mongo/build.gradle.kts
@@ -4,6 +4,6 @@ dependencies {
     api(project(":commons-context"))
     api("com.github.jntakpe:mongo-reactor-adapter:0.1.2")
     api("io.micronaut.mongodb:micronaut-mongo-reactive")
-    api("org.litote.kmongo:kmongo-reactor-serialization:$kMongoVersion")
+    api("org.litote.kmongo:kmongo-async-serialization:$kMongoVersion")
     implementation("io.micronaut:micronaut-inject")
 }
