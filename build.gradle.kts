1d260c100f7afbd7a12ca7eb4c0a7025f3f24c90
jntakpe <jntakpe@gmail.com>
2020-10-30 13:05:16 +0100

    build: release 0.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ allprojects {
 
 subprojects {
     group = "com.github.jntakpe"
-    version = "0.1.2"
+    version = "0.1.3"
 
     dependencies {
         kapt(platform("io.micronaut:micronaut-bom:$micronautVersion"))
