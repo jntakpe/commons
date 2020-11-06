e4e285e766a61626c6cdc8b5704176dbd456c929
jntakpe <jntakpe@gmail.com>
2020-11-06 21:17:05 +0100

    feat: add commons-management module to add support for Kubernetes probes endpoints

---
+++ commons-management/build.gradle.kts
@@ -0,0 +1,4 @@
+dependencies {
+    api("io.micronaut:micronaut-management")
+    api("io.micronaut:micronaut-http-server-netty")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "commons"
 include("commons-context")
 include("commons-grpc")
 include("commons-grpc-test")
+include("commons-management")
 include("commons-micronaut")
 include("commons-mongo")
 include("commons-mongo-test")
