eaaad24444ff4359b81140e5097e0dd89ca57ff5
jntakpe <jntakpe@gmail.com>
2020-11-24 09:48:11 +0100

    build: bump Kotlin to 1.4.20 and micronaut to 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     idea
     `java-library`
     `maven-publish`
-    val kotlinVersion = "1.4.10"
+    val kotlinVersion = "1.4.20"
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
     kotlin("plugin.allopen") version kotlinVersion
