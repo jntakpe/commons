042af10156488befe4dd57556891fc933e56b403
jntakpe <jntakpe@gmail.com>
2020-11-26 23:07:43 +0100

    build: create module commons-tracing that implements OpenTracing

---
+++ commons-tracing/build.gradle.kts
@@ -0,0 +1,9 @@
+val braveVersion: String by project
+
+dependencies {
+    api(project(":commons-micronaut"))
+    api(platform("io.zipkin.brave:brave-bom:$braveVersion"))
+    api("io.zipkin.brave:brave-context-slf4j")
+    api("io.zipkin.brave:brave-instrumentation-grpc")
+    compileOnly("io.grpc:grpc-core")
+}
