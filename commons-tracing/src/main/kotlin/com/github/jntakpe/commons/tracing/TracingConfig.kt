07cf2ab162d3fbdf721eed23078e48ba42366ae9
jntakpe <jntakpe@gmail.com>
2020-11-26 23:23:36 +0100

    feat: configures OpenTracing

---
+++ commons-tracing/src/main/kotlin/com/github/jntakpe/commons/tracing/TracingConfig.kt
@@ -0,0 +1,31 @@
+package com.github.jntakpe.commons.tracing
+
+import brave.Tracing
+import brave.context.slf4j.MDCScopeDecorator
+import brave.handler.SpanHandler
+import brave.propagation.ThreadLocalCurrentTraceContext
+import brave.sampler.Sampler
+import com.github.jntakpe.availability.config.tracing.TracingInvocationInstrumenter
+import io.micronaut.context.annotation.Bean
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.env.Environment
+import io.micronaut.runtime.ApplicationConfiguration
+import javax.inject.Singleton
+
+@Factory
+class TracingConfig(private val appConfig: ApplicationConfiguration) {
+
+    @Singleton
+    @Bean(preDestroy = "close")
+    fun braveTracing(): Tracing {
+        return Tracing.newBuilder()
+            .localServiceName(appConfig.name.orElse(Environment.DEFAULT_NAME))
+            .addSpanHandler(SpanHandler.NOOP)
+            .sampler(Sampler.NEVER_SAMPLE)
+            .currentTraceContext(ThreadLocalCurrentTraceContext.newBuilder().addScopeDecorator(MDCScopeDecorator.get()).build())
+            .build()
+    }
+
+    @Singleton
+    fun tracingInstrumenter(tracing: Tracing) = TracingInvocationInstrumenter(tracing)
+}
