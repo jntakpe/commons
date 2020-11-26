63094ae30488a8d153e6c33141a17264dae52848
jntakpe <jntakpe@gmail.com>
2020-11-26 23:19:48 +0100

    feat: creates micronaut tracing instrumenter

---
+++ commons-tracing/src/main/kotlin/com/github/jntakpe/commons/tracing/TracingInvocationInstrumenter.kt
@@ -0,0 +1,16 @@
+package com.github.jntakpe.availability.config.tracing
+
+import brave.Tracing
+import io.micronaut.scheduling.instrument.Instrumentation
+import io.micronaut.scheduling.instrument.InvocationInstrumenter
+import io.micronaut.scheduling.instrument.ReactiveInvocationInstrumenterFactory
+
+class TracingInvocationInstrumenter(tracing: Tracing) : ReactiveInvocationInstrumenterFactory {
+
+    private val currentTraceContext = tracing.currentTraceContext()
+
+    override fun newReactiveInvocationInstrumenter(): InvocationInstrumenter? {
+        return currentTraceContext.get()
+            ?.run { InvocationInstrumenter { Instrumentation { currentTraceContext.maybeScope(this).close() } } }
+    }
+}
