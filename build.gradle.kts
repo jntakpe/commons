30c0c70942bd3848c9ca0b428b3c524d71ec8bb6
jntakpe <jntakpe@gmail.com>
2020-11-27 19:54:43 +0100

    build: release 0.1.13-RC2

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ allprojects {
 
 subprojects {
     group = "com.github.jntakpe"
-    version = "0.1.13-RC1"
+    version = "0.1.13-RC2"
 
     dependencies {
         kapt(platform("io.micronaut:micronaut-bom:$micronautVersion"))
