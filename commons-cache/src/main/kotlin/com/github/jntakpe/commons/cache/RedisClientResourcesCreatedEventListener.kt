9069e5936b2b5e7d56b304e2871af81e0586eec8
jntakpe <jntakpe@gmail.com>
2020-12-02 14:08:08 +0100

    feat: conditionally load both Redis and Mongo OpenTracing instrumentation. Implies both instrumentation are not loaded during test phase to avoid issues

--- commons-cache/src/main/kotlin/com/github/jntakpe/commons/cache/RedisClientResourcesCreatedEventListener.kt
+++ commons-cache/src/main/kotlin/com/github/jntakpe/commons/cache/RedisClientResourcesCreatedEventListener.kt
@@ -4,12 +4,13 @@ import brave.Tracing
 import io.lettuce.core.resource.ClientResources
 import io.lettuce.core.tracing.BraveTracing
 import io.micronaut.context.annotation.Requires
+import io.micronaut.context.env.Environment
 import io.micronaut.context.event.BeanCreatedEvent
 import io.micronaut.context.event.BeanCreatedEventListener
 import javax.inject.Singleton
 
 @Singleton
-@Requires(beans = [Tracing::class])
+@Requires(beans = [Tracing::class], notEnv = [Environment.TEST])
 class RedisClientResourcesCreatedEventListener(private val tracing: Tracing) : BeanCreatedEventListener<ClientResources> {
 
     override fun onCreated(event: BeanCreatedEvent<ClientResources>): ClientResources {
--- commons-mongo/src/main/kotlin/com/github/jntakpe/commons/mongo/MongoConfigurationCreatedEventListener.kt
+++ commons-mongo/src/main/kotlin/com/github/jntakpe/commons/mongo/MongoConfigurationCreatedEventListener.kt
@@ -4,12 +4,13 @@ import brave.Tracing
 import brave.mongodb.MongoDBTracing
 import io.micronaut.configuration.mongo.core.AbstractMongoConfiguration
 import io.micronaut.context.annotation.Requires
+import io.micronaut.context.env.Environment
 import io.micronaut.context.event.BeanCreatedEvent
 import io.micronaut.context.event.BeanCreatedEventListener
 import javax.inject.Singleton
 
 @Singleton
-@Requires(beans = [Tracing::class])
+@Requires(beans = [Tracing::class], notEnv = [Environment.TEST])
 class MongoConfigurationCreatedEventListener(private val tracing: Tracing) : BeanCreatedEventListener<AbstractMongoConfiguration> {
 
     override fun onCreated(event: BeanCreatedEvent<AbstractMongoConfiguration>): AbstractMongoConfiguration {
