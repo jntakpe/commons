e777661d01f53722a9a65a5693a4de04f005090a
jntakpe <jntakpe@gmail.com>
2020-12-02 12:07:34 +0100

    feat: remove grpc tracing config from commons-tracing module to delegate grpc tracing config to commons-grpc

--- commons-tracing/build.gradle.kts
+++ commons-tracing/build.gradle.kts
@@ -1,9 +1,6 @@
-val braveVersion: String by project
-
 dependencies {
     api(project(":commons-context"))
     api(project(":commons-micronaut"))
-    api(platform("io.zipkin.brave:brave-bom:$braveVersion"))
     api("io.zipkin.brave:brave-context-slf4j")
     api("io.zipkin.brave:brave-instrumentation-grpc")
     compileOnly("io.grpc:grpc-core")
