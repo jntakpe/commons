8774749b9d0dd9cad23065b37ba3acb0fe71e572
jntakpe <jntakpe@gmail.com>
2020-12-02 12:54:00 +0100

    feat: add support for Redis OpenTracing

--- commons-cache/build.gradle.kts
+++ commons-cache/build.gradle.kts
@@ -4,4 +4,5 @@ dependencies {
     api(project(":commons-context"))
     api("io.micronaut.redis:micronaut-redis-lettuce")
     implementation("io.micronaut:micronaut-inject")
+    compileOnly("io.zipkin.brave:brave")
 }
---
+++ commons-cache/src/main/kotlin/com/github/jntakpe/commons/cache/RedisClientResourcesCreatedEventListener.kt
@@ -0,0 +1,18 @@
+package com.github.jntakpe.commons.cache
+
+import brave.Tracing
+import io.lettuce.core.resource.ClientResources
+import io.lettuce.core.tracing.BraveTracing
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.event.BeanCreatedEvent
+import io.micronaut.context.event.BeanCreatedEventListener
+import javax.inject.Singleton
+
+@Singleton
+@Requires(beans = [Tracing::class])
+class RedisClientResourcesCreatedEventListener(private val tracing: Tracing) : BeanCreatedEventListener<ClientResources> {
+
+    override fun onCreated(event: BeanCreatedEvent<ClientResources>): ClientResources {
+        return event.bean.mutate().tracing(BraveTracing.create(tracing)).build()
+    }
+}
