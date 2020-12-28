cdc6a865ef46c649178ba2c1c7f2f61098a47bac
jntakpe <jntakpe@gmail.com>
2020-12-28 21:27:39 +0100

    build: bump Kotlin version to 1.4.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     idea
     `java-library`
     `maven-publish`
-    val kotlinVersion = "1.4.20"
+    val kotlinVersion = "1.4.21"
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
     kotlin("plugin.allopen") version kotlinVersion
