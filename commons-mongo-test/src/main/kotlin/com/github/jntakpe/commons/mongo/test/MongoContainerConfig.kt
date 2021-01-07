caabc2cd736bb0157f6de331e04ad0a456426182
jntakpe <jntakpe@gmail.com>
2021-01-07 20:11:31 +0100

    feat: configures Mongo client using bean declaration rather than an Event Listener to avoid Mongo health indicator not being properly configured

--- commons-mongo-test/src/main/kotlin/com/github/jntakpe/commons/mongo/test/MongoContainerConfig.kt
+++ commons-mongo-test/src/main/kotlin/com/github/jntakpe/commons/mongo/test/MongoContainerConfig.kt
@@ -1,10 +1,8 @@
 package com.github.jntakpe.commons.mongo.test
 
 import com.github.jntakpe.commons.context.logger
-import com.mongodb.reactivestreams.client.MongoClient
-import com.mongodb.reactivestreams.client.MongoClients
+import com.mongodb.MongoClientSettings
 import io.micronaut.configuration.mongo.core.DefaultMongoConfiguration
-import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Replaces
 import javax.inject.Singleton
@@ -15,13 +13,11 @@ class MongoContainerConfig {
     private val log = logger()
 
     @Singleton
-    @Bean(preDestroy = "close")
-    @Replaces(bean = MongoClient::class)
-    fun mongoContainerClient(initConfig: DefaultMongoConfiguration): MongoClient {
+    @Replaces(bean = MongoClientSettings::class)
+    fun testMongoSettings(initConfig: DefaultMongoConfiguration): MongoClientSettings {
         val container = MongoContainer.instance
         val mongoUri = "mongodb://${container.containerIpAddress}:${container.firstMappedPort}"
         log.debug("Setting MongoDB container URI to $mongoUri")
-        val testConfig = initConfig.apply { uri = mongoUri }
-        return MongoClients.create(testConfig.buildSettings())
+        return initConfig.apply { uri = mongoUri }.buildSettings()
     }
 }
--- commons-mongo/src/main/kotlin/com/github/jntakpe/commons/mongo/MongoConfig.kt
+++ commons-mongo/src/main/kotlin/com/github/jntakpe/commons/mongo/MongoConfig.kt
@@ -1,8 +1,14 @@
 package com.github.jntakpe.commons.mongo
 
+import brave.Tracing
 import com.mongodb.MongoClientSettings
 import com.mongodb.reactivestreams.client.MongoClient
+import com.mongodb.reactivestreams.client.MongoClients
+import com.mongodb.reactor.client.tracing.TracingReactorMongoClient
+import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.context.annotation.Requires
 import org.litote.kmongo.reactivestreams.withKMongo
 import org.litote.kmongo.serialization.changeIdController
 import javax.inject.Singleton
@@ -16,4 +22,10 @@ class MongoConfig(private val settings: MongoClientSettings) {
 
     @Singleton
     fun reactorDatabase(client: MongoClient) = client.getDatabase(settings.applicationName).withKMongo()
+
+    @Singleton
+    @Bean(preDestroy = "close")
+    @Requires(beans = [Tracing::class])
+    @Replaces(bean = MongoClient::class)
+    fun tracingClient(settings: MongoClientSettings, tracing: Tracing) = TracingReactorMongoClient(MongoClients.create(settings), tracing)
 }
