b607cdbe1473b70df02ce86e421d817ef9b609ac
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-02-03 22:07:05 +0100

    build: release version 0.2.0-RC1

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ allprojects {
 
 subprojects {
     group = "com.github.jntakpe"
-    version = "0.1.16"
+    version = "0.2.0-RC1"
 
     dependencies {
         kapt(enforcedPlatform("io.micronaut:micronaut-bom:$micronautVersion"))
