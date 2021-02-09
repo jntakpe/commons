ac3751a59c2acbd697814ae227933c40af62d4eb
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-02-09 20:50:20 +0000

    build: add jul to slf4j dependency and force slf4j version

--- commons-context/build.gradle.kts
+++ commons-context/build.gradle.kts
@@ -1,5 +1,7 @@
+val slf4jVersion: String by project
+
 dependencies {
-    api("org.slf4j:slf4j-api")
+    api("org.slf4j:slf4j-api:$slf4jVersion")
     api("io.grpc:grpc-api")
     api("io.projectreactor:reactor-core")
     api("io.projectreactor.kotlin:reactor-kotlin-extensions")
--- commons-tracing/build.gradle.kts
+++ commons-tracing/build.gradle.kts
@@ -1,6 +1,9 @@
+val slf4jVersion: String by project
+
 dependencies {
     api(project(":commons-context"))
     api(project(":commons-micronaut"))
     api("io.zipkin.brave:brave-context-slf4j")
     api("io.zipkin.brave:brave-instrumentation-grpc")
+    api("org.slf4j:jul-to-slf4j:$slf4jVersion")
 }
