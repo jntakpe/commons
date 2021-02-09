146944c4910ddee2072061904821cecda9bae948
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-02-09 20:45:20 +0000

    build: bump reactor mongo adapter version to 0.2.0-RC5

--- commons-mongo/build.gradle.kts
+++ commons-mongo/build.gradle.kts
@@ -1,8 +1,9 @@
 val kMongoVersion: String by project
+val mongoReactionAdapterVersion: String by project
 
 dependencies {
     api(project(":commons-context"))
-    api("com.github.jntakpe:tracing-mongo-reactor-adapter:0.2.0-RC4")
+    api("com.github.jntakpe:tracing-mongo-reactor-adapter:$mongoReactionAdapterVersion")
     api("io.micronaut.mongodb:micronaut-mongo-reactive")
     api("org.litote.kmongo:kmongo-reactor-serialization:$kMongoVersion")
     implementation("io.micronaut:micronaut-inject")
